105afcbe83ab46d2b8a5fab90361959d4c9db8ec
gnandeepgottipati <gnangottipati2001@gmail.com>
2020-02-07 16:31:03 -0500

    Completed some of the server Protocols

--- app/src/main/java/com/example/loops/util/Protocols.java
+++ app/src/main/java/com/example/loops/util/Protocols.java
@@ -1,4 +1,25 @@
 package com.example.loops.util;
 
 public interface Protocols {
+
+    /**
+     * From: server
+     * To: client
+     * Informs the client that he/she is connected to the server
+     */
+    public static final String WELCOME = "WELCOME";
+
+    /**
+     * From: client
+     * To: Server
+     * Requests the server to send the message
+     */
+    public static String SEND = "SEND";
+
+    /**
+     * From : server
+     * To : client
+     * Notifies the client that he/she received a new loop
+     */
+    public static String RECEIVE = "RECEIVE";
 }
